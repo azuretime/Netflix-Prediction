598330d3d5dd131bac03e47409f1b2951d194716
Jiang, Jinjing <Jinjing.Jiang@mastercard.com>
2023-04-22 14:33:30 +0800

    display poster

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Response, Response, Response, Response, Flask, render_template, request, jsonify, send_from_directory, url_for
-from flask_uploads import UploadSet,IMAGES, configure_uploads
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileRequired, FileField, FileAllowed
-from wtforms import SubmitField
+from flask import Flask, redirect, render_template, request, url_for
+from werkzeug.utils import secure_filename
 import os
 import numpy as np
 import yaml
 import joblib 
+import logging
 
 webapp_root = "webapp"
 params_path = "params.yaml"
@@ -15,26 +13,7 @@ static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
-app.config['SECRET_KEY'] = 'keyfjdchtfjyu'
-app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
-
-photos = UploadSet('photos',IMAGES)
-configure_uploads(app, photos)
-
-global response
-response = 'response'
-global file_url
-file_url = 'file_url'
-
-class UploadForm(FlaskForm):
-    photo = FileField(
-        validators=[
-            FileAllowed(photos, 'Only images are allowed'),
-            FileRequired('File field should not be empty')
-        ]
-    )
-    submit = SubmitField('Upload')
-
+app.config['UPLOAD_FOLDER'] = static_dir + '/uploads/'
 
 class  Emptymessage(Exception):
     def __init__(self, message="Receive empty data"):
@@ -46,14 +25,14 @@ def read_params(config_path):
         config = yaml.safe_load(yaml_file)
     return config
 
-def predict(data):
-    config = read_params(params_path)
-    label_map = config["raw_data_config"]['label_encoding']
-    model_dir_path = config["model_webapp_dir"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data).tolist()[0]
-    str_prediction = [k for k,v in label_map.items() if v == prediction][0]
-    return str_prediction 
+# def predict(data):
+#     config = read_params(params_path)
+#     label_map = config["raw_data_config"]['label_encoding']
+#     model_dir_path = config["model_webapp_dir"]
+#     model = joblib.load(model_dir_path)
+#     prediction = model.predict(data).tolist()[0]
+#     str_prediction = [k for k,v in label_map.items() if v == prediction][0]
+#     return str_prediction 
 
 def validate_input(dict_request):
     for _, val in dict_request.items():
@@ -65,68 +44,38 @@ def form_response(dict_request):
     try:
         if validate_input(dict_request):
             data = dict_request.values()
-            response = 'predict(data)'
+            response = 'predict(data 7658765876576)'
             return response
     except Emptymessage as e:
         response =  str(e)
         return response 
 
-# @app.route("/", methods=["GET", "POST"])
-# def index():
-#     if request.method == "POST":
-#         try:
-#             if request.form:
-#                 dict_req = dict(request.form)
-#                 response = form_response(dict_req)
-#                 return render_template("index.html", response=response)
-#         except Exception as e:
-#             print(e)
-#             error = {"error": "Something went wrong!! Try again later!"}
-#             error = {"error": e}
-#             return render_template("404.html", error=error)
-#     else:
-#         return render_template("index.html")
-
-@app.route('/uploads/<filename>')
-def get_image(filename):
-    return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'], filename)
-
 @app.route("/", methods=["GET", "POST"])
-def upload_image():
-    form = UploadForm()
-    if form.validate_on_submit():
-        filename = photos.save(form.photo.data)
-        file_url = url_for('get_image', filename = filename)
+def index():
+    if request.method == "POST":
+        try:
+            if request.form and ('file' in request.files):
+                dict_req = dict(request.form)
+                response = form_response(dict_req)
+
+                image = request.files['file']
+                if image.filename != '':
+                    filename = secure_filename(image.filename)
+                    logging.info('upload_image filename: ' + filename)
+                    image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return render_template("index.html", response=response, filename= filename)
+        except Exception as e:
+            print(e)
+            error = {"error": "Something went wrong!! Try again later!"}
+            error = {"error": e}
+            return render_template("404.html", error=error)
     else:
-        file_url = None 
+        return render_template("index.html")
+    return render_template("index.html")
 
-    # if request.form:
-    #     dict_req = dict(request.form)
-    #     global response
-    #     response = form_response(dict_req)
-
-    return render_template("index.html", form=form, file_url = file_url)
-    # if request.method == "POST":
-    #     try:
-    #         form = UploadForm()
-    #         if form.validate_on_submit():
-    #             filename = photos.save(form.photo.data)
-    #             file_url = url_for('get_image', filename = filename)
-    #         else:
-    #             file_url = None 
-
-    #         if request.form:
-    #             dict_req = dict(request.form)
-    #             response = form_response(dict_req)
-    #             return render_template("index.html", response=response)
-    #         return render_template("index.html", form=form, file_url = file_url)
-    #     except Exception as e:
-    #         print(e)
-    #         error = {"error": "Something went wrong!! Try again later!"}
-    #         error = {"error": e}
-    #         return render_template("404.html", error=error)
-    # else:
-    #     return render_template("index.html")    
+@app.route('/display/<filename>', methods=["GET"])
+def display_image(filename):
+    return redirect(url_for('static', filename = '/uploads/' + filename), code=301)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
